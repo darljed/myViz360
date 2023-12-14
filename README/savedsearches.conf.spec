79a574fc527b57407b2efb3e75d59caab23fd97c
Darl Jed Matundan <djam.devteam@gmail.com>
2023-12-14 22:39:51 +0800

    Initial working version. Contains singlevalue360 and singlevalue mini

---
+++ README/savedsearches.conf.spec
@@ -0,0 +1,14 @@
+# Declare properties here
+
+
+# General 
+display.visualizations.custom.myViz360.singlevalue360.grouping = <string>
+display.visualizations.custom.myViz360.singlevalue360.groupPosition = <string>
+
+
+# Setup 
+display.visualizations.custom.myViz360.singlevalue360.upcolor = <string>
+display.visualizations.custom.myViz360.singlevalue360.downcolor = <string>
+
+display.visualizations.custom.myViz360.singlevaluemini.upcolor = <string>
+display.visualizations.custom.myViz360.singlevaluemini.downcolor = <string>
---
+++ appserver/static/visualizations/singlevaluemini/src/visualization_source.js
@@ -0,0 +1,87 @@
+/*
+ * Visualization source
+ */
+
+define([
+            'jquery',
+            'underscore',
+            'api/SplunkVisualizationBase',
+            'api/SplunkVisualizationUtils'
+            // Add required assets to this list
+        ],
+        function(
+            $,
+            _,
+            SplunkVisualizationBase,
+            vizUtils
+        ) {
+  
+    // Extend from SplunkVisualizationBase
+    return SplunkVisualizationBase.extend({
+  
+        initialize: function() {
+            SplunkVisualizationBase.prototype.initialize.apply(this, arguments);
+            this.$el = $(this.el);
+            this.$el.css('min-height','150px')
+            this.$el.addClass('cc-single-value-mini-main')
+            // //console.log(this.$el)
+
+            this.options = {
+                theme: vizUtils.getCurrentTheme(),
+            }
+            // Initialization logic goes here
+        },
+
+        // Optionally implement to format data returned from search. 
+        // The returned object will be p`assed to updateView as 'data'
+        formatData: function(data) {
+            console.log(data)
+            // Format data 
+            
+            return data
+        },
+  
+        // Implement updateView to render a visualization.
+        //  'data' will be the data object returned from formatData or from the search
+        //  'config' will be the configuration property object
+        updateView: function(data, config) {
+            //console.log(data)
+            // Draw something here
+            
+            this.$el.html('')
+
+            this.style ={
+                upcolor: config[this.getPropertyNamespaceInfo().propertyNamespace + 'upcolor'] || 'green',
+                downcolor: config[this.getPropertyNamespaceInfo().propertyNamespace + 'downcolor'] || 'red',
+            }
+
+            let items =  ``
+            data.rows.forEach(element => {
+              items+=`<div class="cc-mini-sv-item">
+              <div class="cc-mini-sv-item-label">${element[0]}</div>
+              <span>|</span>
+              <div class="cc-mini-sv-item-value ${element[2]} ${element[2] == 'up' ? this.style.upcolor : ( element[2] == 'down' ? this.style.downcolor : '')}">${element[1]} <i class="icon icon-arrow-right"/>
+              </div>
+            </div>`
+            });
+
+            this.$el.append(`<div class="cc-mini-sv">
+            ${items}
+          </div>`)
+
+
+
+        },
+
+        // Search data params
+        getInitialDataParams: function() {
+            return ({
+                outputMode: SplunkVisualizationBase.ROW_MAJOR_OUTPUT_MODE,
+                count: 10000
+            });
+        },
+
+        // Override to respond to re-sizing events
+        reflow: function() {}
+    });
+});
